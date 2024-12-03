30462be1318b16cbcf292a1c454e6eff073c9beb
zulema <zcetz@comcast.net>
2024-12-03 15:28:46 -0800

    GameOverScreen

--- 180_Final/Assets/Scripts/MainMenuUI.cs
+++ 180_Final/Assets/Scripts/MainMenuUI.cs
@@ -5,18 +5,22 @@ using UnityEngine.SceneManagement;
 /// <summary>
 /// Cetz, Zulema 
 /// 12/3/24
-/// Handles the main menu UI 
+/// Handles the main menu UI and game over screen
 /// </summary>
 public class MainMenuUI : MonoBehaviour
 {
 
-    public void QuitGame()
-    {
-        Application.Quit();
-    }
-
+    public GameObject playText;
+    public GameObject quitText;
+   
     public void SwitchScene(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
+    }
+    // Quits the game when the ui button for is clicked
+    public void Quit()
+    {
+        print("Quit Game");
+        Application.Quit();
     }
 }
