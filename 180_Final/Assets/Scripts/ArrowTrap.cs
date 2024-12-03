3e08e24820ac732da233056e4b4d6171e1360c20
ariannare05 <arisaf2005@gmail.com>
2024-12-03 14:57:33 -0800

    Created Traps Test Scene

---
+++ 180_Final/Assets/Scripts/ArrowTrap.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ * Name: Arianna Reyes
+ * Date: 12/3/2024
+ * This will handle the arrow trap functions
+ */
+public class ArrowTrap : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 180_Final/Assets/Scripts/BreakableFloor.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ * Name: Arianna Reyes
+ * Date: 12/3/2024
+ * This handles the function of the breakable floor trap
+ */
+public class BreakableFloor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
