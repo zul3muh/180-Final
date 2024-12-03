83343b12c52a3538dad395fa57c443f9ac18ba06
zulema <zcetz@comcast.net>
2024-12-03 15:02:55 -0800

    MainMenuUI

---
+++ 180_Final/Assets/Scripts/MainMenuUI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+/// <summary>
+/// Cetz, Zulema 
+/// 12/3/24
+/// Handles the main menu UI 
+/// </summary>
+public class MainMenuUI : MonoBehaviour
+{
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void SwitchScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
