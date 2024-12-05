93b4527c31d8a0edfb6e8583b56395707a87fd6c
Matt3324 <jimenezmateo3324@gmail.com>
2024-12-04 23:25:10 -0800

    created player control scripts
    a

---
+++ 180_Final/Assets/Scripts/MPlayer_Controller.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MPlayer_Controller : MonoBehaviour
+{
+    /* Mateo Jimenez
+     * 12/3/2024
+     * handles player movement 
+     */
+
+    public float sensX;
+    public float sensY;
+
+    public Transform orientation;
+
+    float xRotation;
+    float yRotation;
+
+
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+
+        yRotation += mouseX;
+
+        xRotation -= mouseY;
+
+        //limits vertical rotation
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        orientation.rotation = Quaternion.Euler(0, yRotation, 0);   
+    }
+
+}
